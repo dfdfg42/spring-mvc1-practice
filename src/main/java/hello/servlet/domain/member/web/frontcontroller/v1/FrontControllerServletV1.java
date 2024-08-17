70c87e35aa996b591969f85a002faa7707c009f5
고경우 <dfdfg11@gmail.com>
2024-08-17 10:09:25 +0900

    mvc-controllerv2

--- src/main/java/hello/servlet/domain/member/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/domain/member/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -2,7 +2,8 @@ package hello.servlet.domain.member.web.frontcontroller.v1;
 
 import hello.servlet.domain.member.web.frontcontroller.v1.controller.MemberFormControllerV1;
 import hello.servlet.domain.member.web.frontcontroller.v1.controller.MemberListControllerV1;
-import hello.servlet.domain.member.web.frontcontroller.v1.controller.MemberSvaeControllerV1;
+import hello.servlet.domain.member.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+import hello.servlet.domain.member.web.frontcontroller.v1.controller.MemberSaveControllerV1;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -21,7 +22,7 @@ public class FrontControllerServletV1 extends HttpServlet {
 
     public FrontControllerServletV1() {
         controllerMap.put("/front-controller/v1/members/new-form",new MemberFormControllerV1());
-        controllerMap.put("/front-controller/v1/members/save",new MemberSvaeControllerV1());
+        controllerMap.put("/front-controller/v1/members/save",new MemberSaveControllerV1());
         controllerMap.put("/front-controller/v1/members",new MemberListControllerV1());
 
     }
